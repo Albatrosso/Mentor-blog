3e40aed033c1fbc33d03e30a58886349cfbc660d
Никита Павленко <nick.pavlenko1998@gmail.com>
2023-06-06 01:41:27 +0300

    исправление типов

--- src/components/mentyListItem.tsx
+++ src/components/mentyListItem.tsx
@@ -1,6 +1,6 @@
 import MentyStars from "./mentyStars"
 import AvatarUI from "./ui/avatar"
-export default function MentyListItem({menty}) {
+export default function MentyListItem({menty}:{menty: any}) {
   return(<div className="rounded border-2 border-purple-200 w-1/2 min-w-min flex flex-row justify-between p-1">
     <div className="flex flex-row w-1/2">
       <AvatarUI imgSrc={menty.image}/>
